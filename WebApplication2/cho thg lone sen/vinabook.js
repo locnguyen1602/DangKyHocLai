1edd48ca0d5f94b821cf46ce68be8aee54e7c6d9
Loc ngyen <nploc1602@gmail.com>
2021-01-18 22:56:28 +0700

    Add project files.

---
+++ WebApplication2/cho thg lone sen/vinabook.js
@@ -0,0 +1,24 @@
+﻿
+function MouseSeen(elm)
+{
+    $("." + elm + "-content").addClass("show");
+}
+function MouseOut(elm)
+{
+    $("." + elm + "-content").removeClass("show");
+}
+$('[elm="vn"]').mouseenter(function()
+{
+    MouseSeen($(this).attr("class"));
+});
+$('[elm="vn"]').mouseleave(function () {
+    MouseOut($(this).attr("class"));
+});
+window.onscroll  = function Scroll()
+{
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        $(".d3").addClass("scrollmouse");
+    } else {
+        $(".d3").removeClass("scrollmouse");
+    }
+}
